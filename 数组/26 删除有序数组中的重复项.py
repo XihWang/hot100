d9ed7de8d9742429d219262883c54e788378e3e5
Glycogen W <109408857+XihWang@users.noreply.github.com>
2025-07-22 21:25:50 +0800

    Add files via upload

---
+++ 数组/26 删除有序数组中的重复项.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 1:
+            return len(nums)
+        slow = 0
+        fast = 1
+        while fast < len(nums):
+            if nums[slow] != nums[fast]:
+                slow += 1
+                nums[slow] = nums[fast]
+            fast += 1
+        return slow+1
