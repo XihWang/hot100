27ce9ab033dd12b72cc1c4a13017e26a2e676f74
Glycogen W <109408857+XihWang@users.noreply.github.com>
2025-07-24 23:00:09 +0800

    Add files via upload

---
+++ 数组/189 轮转数组.py
@@ -0,0 +1,26 @@
+# from typing import List
+# class Solution:
+#     def rotate(self, nums: List[int], k: int) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         n = len(nums)
+#         k = k % n
+#         nums[:] = nums[-k:] + nums[:-k]
+
+from typing import List
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        def reverse(nums, i, j):
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+        reverse(nums, 0, n - 1)
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, n - 1)
